f9f54f31df0d4b5a41876fc552ca6b236968ac62
Mihainuan de Sá Silva <mndv@gft.com>
2022-04-04 14:50:31 -0300

    First implementations - added connection string - added Infectado class - added MongoDB Driver

---
+++ Data/Collections/Infectado.cs
@@ -0,0 +1,20 @@
+using System;
+using MongoDB.Driver.GeoJsonObjectModel;
+
+namespace ApiMongoDB.Data.Collections
+{
+    public class Infectado
+    {
+        public Infectado(DateTime dataNascimento, string genero, double latitude, double longitude)
+        {
+            this.DataNascimento = dataNascimento;
+            this.Genero = genero;
+            this.Localizacao = new GeoJson2DGeographicCoordinates(longitude,latitude);
+        }
+        public DateTime DataNascimento { get; set; }
+
+        public string Genero { get; set; }
+
+        public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+    }
+}
